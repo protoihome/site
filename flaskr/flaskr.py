d26a37c8ece9c2e5b5240c2ce545e462b0a43d4f
Pedro <pedrostm38@gmail.com>
2017-04-05 17:11:57 -0300

    bug

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 ###################################################################     
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ihome.sqlite3'
 app.config['SECRET_KEY'] = "random string"
 db = SQLAlchemy(app)
 ####################################################################
