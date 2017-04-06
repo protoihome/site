31005c5bc9f31813c80fb6dca15f17f90124b752
Pedro <pedrostm38@gmail.com>
2017-04-06 11:32:52 -0300

    teste

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -82,7 +82,7 @@ def devices():
 @app.route('/room')
 def room():
     #c = Rooms.query.all()
-    dicionario = {{'comodo':'Sala'}"aparelhos": [{"pin": 13,"id":1, "nome": "Lampada 1", "status":0}]}
+    dicionario = {{'comodo':'Sala'},"aparelhos": [{"pin": 13,"id":1, "nome": "Lampada 1", "status":0}]}
     #return json.dumps(c, cls=AlchemyEncoder)
     return dicionario
 	#return jsonify(room=[dict(id=1,nome='sala'),  dict(id=2,nome='quarto')])
