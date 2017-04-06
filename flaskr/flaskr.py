9c7ce26139a1c56c27831e295dd0f1c661138ae9
sandro <ssillva@bol.com.br>
2017-04-06 20:41:15 -0300

    pinagem repete3

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -27,7 +27,7 @@ class Rooms(db.Model):
 
 class Devices(db.Model):
     id_ = db.Column(db.Integer, primary_key=True)
-    pin = db.Column(db.String(50))
+    pin = db.Column(db.Integer)
     name = db.Column(db.Integer)
     status = db.Column(db.Integer)
     id_room = db.Column(db.Integer, db.ForeignKey('rooms.id_'))
